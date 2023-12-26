05ca4cea383f0d57324a7a50dbfc968fcda498c5
Richard <richard.skoda72@gmail.com>
2023-12-26 16:53:27 +0100

    delete initial vue site; begin lections

---
+++ src/components/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> {{ product }} </h1>
+        <p> {{ description }} </p>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const product = ref('Shoes')
+const description = ref('Blue')
+
+
+</script>
+
+<style lang="scss">
+
+</style>
